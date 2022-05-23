372c87087ab7fae5a0605429fcca0958647e1400
Thai Xiong <thai303@hotmail.com>
2022-05-22 21:27:41 -0600

    commit tree 0099f7db50a287334d53e9c635d0fd47f7daabe6

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+document.addEventListener( "DOMContentLoaded", function () {
+    updateDOM()
+  } );
+function updateDOM() {
+    document.getElementById("text").innerHTML = "This is really cool!";
+}
